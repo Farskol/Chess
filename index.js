56082e9cf720685c27c68857ea934261832f5a3c
Farskol <fransresto@gmail.com>
2022-11-28 17:33:18 +0300

    socet

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const port = process.env.PORT || 3001;
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
@@ -17,6 +18,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3001, () => {
-    console.log('listening on *:3001');
+server.listen(port, () => {
+    console.log(`listening on *:${port}`);
 });
