779b67b30ed4c989d8fa14b086d7638bc63abb2f
Farskol <fransresto@gmail.com>
2022-12-26 13:13:58 +0300

    create new logs

--- index.js
+++ index.js
@@ -90,12 +90,12 @@ io.on('connection', (socket) => {
 
     socket.on("disconnect", (reason) =>{
 
-        for(let i = 0; i < pullOfGames; i++){
+        for(let i = 0; i < pullOfGames.length; i++){
             if (pullOfGames[i].firstPlayer.socketId === socket.id){
-                console.log(pullOfGames[i].firstPlayer.first_name + "disconnected by ->" + reason.toString())
+                console.log(pullOfGames[i].firstPlayer.first_name + " disconnected by ->" + reason.toString())
             }
             if(pullOfGames[i].secondPlayer.socketId === socket.id){
-                console.log(pullOfGames[i].secondPlayer.first_name + "disconnected by ->" + reason.toString())
+                console.log(pullOfGames[i].secondPlayer.first_name + " disconnected by ->" + reason.toString())
             }
         }
 
