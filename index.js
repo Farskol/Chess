25b30b279307d4a010f5864620d31ddbb3c26f97
Работа <hosnateymori85741@gmail.com>
2023-03-27 13:16:07 +0300

    new commit

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const port = 5040;
-//process.env.PORT ||
+const port = process.env.PORT || 5040;
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
